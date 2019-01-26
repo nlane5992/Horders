b538920b02dbab6c26999902a9983a2949a1d53f
RyanW <rwinter91@gmail.com>
2019-01-25 22:34:39 -0500

    Adding initial project

---
+++ Source/ActionRPG/Private/RPGGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "RPGGameModeBase.h"
+#include "RPGGameStateBase.h"
+#include "RPGPlayerControllerBase.h"
+
+ARPGGameModeBase::ARPGGameModeBase()
+{
+	GameStateClass = ARPGGameStateBase::StaticClass();
+	PlayerControllerClass = ARPGPlayerControllerBase::StaticClass();
+}
